67572489a0635b5ba54752a1897fa0adcac30918
Ousama Kassimi <ousamakassimi02@gmail.com>
2025-01-22 09:45:50 +0100

    Hemos creado el enumerado ficha con los valores verde y azul

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public enum Ficha {
+    VERDE,
+    AZUL;
+
+    @Override
+    public String toString() {
+        return name();
+    }
+}
